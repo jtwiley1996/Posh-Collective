99808825a99c3f50468bfcab416d4686cfdc51b7
jtwiley1996 <98716163+jtwiley1996@users.noreply.github.com>
2024-07-29 17:41:26 -0500

    still adjusting and fixing header

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -1,11 +1,12 @@
-// client/src/Header.jsx
-
 import React from 'react';
 import { Link } from 'react-router-dom'; // If you're using React Router for navigation
 
 const Header = () => {
   return (
     <header>
+      <div className="header-left">
+        <span className="logo-text">P O S H  C O L L E C T I V E</span>
+      </div>
       <nav>
         <ul>
           <li>
