81f726b67231878c026ca7d4e5990335543e2d24
jtwiley1996 <98716163+jtwiley1996@users.noreply.github.com>
2024-08-05 20:19:58 -0500

    owner section of About.jsx

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import '../styling/About.css'; // Assuming you'll add some CSS for styling
+import PoshStoreFront from '../images/PoshStoreFront.png';
+import PoshOwners from '../images/PoshOwners.png';
 
 const About = () => {
   return (
     <div className="about-container">
       <h2 className="about-title">About Us</h2>
       <p className="about-description">Learn more about our company and mission.</p>
-      
+
+      <div className="owner-section">
+        <h3 className="section-title">Meet the Owners</h3>
+        <p className="owner-description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolorum reiciendis? Quam culpa praesentium deleniti rerum amet magnam. Fugiat error autem repellendus, culpa deserunt officiis nam non. Vel, deleniti deserunt!</p>
+        <img src={PoshOwners} alt="Posh Owners" className='owner-picture' />
+      </div>
+
       <div className="location-section">
         <h3 className="section-title">Our Location</h3>
         <p className="location-description">We are located near the intersection of Morris and Capitol, at 1425 E. Capitol Drive in Shorewood.</p>
+        <img src={PoshStoreFront} alt="Posh Store Front" className='location-picture' />
       </div>
       
       <div className="consignment-section">
